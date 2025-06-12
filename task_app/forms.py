a1ea7095d3dfe917b5d01989c7dec93c436dd246
RamonLGDaw <ramonlg.daw@gmail.com>
2025-06-12 09:11:40 +0200

    mejoras form

--- task_app/forms.py
+++ task_app/forms.py
@@ -2,19 +2,20 @@ from django import forms
 from task_app.models import Category
 
 class CreateNewTask(forms.Form):
+    category = forms.ModelChoiceField(
+        label='',
+        queryset=Category.objects.all(),
+        empty_label="Select a category",
+        widget=forms.Select(attrs={
+            'class': 'form-select mt-0 mb-4'
+        })
+    )
     title = forms.CharField(
-        label='Create a new task',
+        label='2. Create a new task',
         max_length=100,
         widget=forms.TextInput(attrs={
             'class': 'form-control mt-2 mb-3',
             'placeholder': 'new task...',
             'maxlength': 100
-        })
-    )
-    category = forms.ModelChoiceField(
-        queryset=Category.objects.all(),
-        empty_label="Select a category",
-        widget=forms.Select(attrs={
-            'class': 'form-select mt-2'
         })
     )
